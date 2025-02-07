19f7dc5fe47fc90942d1acf22f83f857a1b79f3d
himadri553 <himadri553@gmail.com>
2025-02-07 10:07:05 -0500

    Ensured folder paths are correct, fixed TrainModel1

--- FaceDectetionModule/ScikitLearn/LivePhase.py
+++ FaceDectetionModule/ScikitLearn/LivePhase.py
@@ -1,46 +1,35 @@
 """
-    Himadri Saha
-    FaceControlModule.py
+    @ computervisioneng
+    LivePhase.py
 
-    This is the main script for the module. It runs all other scripts and controls the user interface
+    Script for decteting and predicting live face dectetion. Used to brainstorm FaceControlModule.py
 
-    Adapted from:
-    https://github.com/computervisioneng/emotion-recognition-python-scikit-learn-mediapipe/tree/main
-
-    TODO:
-    - Add setup phase, Live phase, and user text prompts
-    - Get setup data into preloaded data
-    - Consider having setup in a seprate script? The app might not need to recalibrate every single use, just when there is a "new user"
-
+    Adapted from: test_model.py
+    https://github.com/computervisioneng/emotion-recognition-python-scikit-learn-mediapipe/blob/main/test_model.py
 """
-
 # Imports
 import pickle
 import cv2
 from utils import get_face_landmarks
 
-# Vars
-emotions = ['HAPPY', 'SAD', 'SURPRISED'] # Change for each catogory
+# Change to match expressions
+emotions = ['HAPPY', 'SAD', 'SURPRISED']
 
-# Load in NN model (fix path)
+# Load in trained model (change path of trained model)
 with open('./model', 'rb') as f:
     model = pickle.load(f)
 
-# Run video capture feed (add setup phase, Live phase, and user text prompts)
+# Main loop to predict live expressions
 cap = cv2.VideoCapture(0)
 ret, frame = cap.read()
 while ret:
-    # Set up phase - Prompt user to make face, press s to take photo, save photo in correspodning folder, and repeat for all expressions #
+    ret, frame = cap.read()
 
+    # Capture live face expressions and predict face expression
+    face_landmarks = get_face_landmarks(frame, draw=True, static_image_mode=False)
+    output = model.predict([face_landmarks])
 
-    # Live phase - #
-    ret, frame = cap.read() # get next frame
-
-    face_landmarks = get_face_landmarks(frame, draw=True, static_image_mode=False) # get face landparks
-
-    output = model.predict([face_landmarks]) # predict using model
-
-    # Display video and text here
+    # Display creted frame
     cv2.putText(frame,
                 emotions[int(output[0])],
                (10, frame.shape[0] - 1),
@@ -48,10 +37,9 @@ while ret:
                3,
                (0, 255, 0),
                5)
-    cv2.imshow('frame', frame) 
+    cv2.imshow('frame', frame)
+    cv2.waitKey(25)
 
-    cv2.waitKey(25) # delay next frame for smoothness
-
-
+# Cleanup
 cap.release()
 cv2.destroyAllWindows()
